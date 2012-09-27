97f1050616478e368272450ad91c7c57593b03d6
Elmer Rivera <elmer@vizcayano.com>
2012-09-28 07:15:39 +0800

    fix test spec

--- modules/stack/manifests/setup.pp
+++ modules/stack/manifests/setup.pp
@@ -2,7 +2,7 @@ class stack::setup {
   notify{"Setting up stack...":}
   ->
   exec {'refresh_apt_cache':
-    #command     => "/usr/bin/apt-get -q update",
-    command     => "/bin/true || /usr/bin/apt-get -q update",
+    command     => "/usr/bin/apt-get -q update",
+    #command     => "/bin/true || /usr/bin/apt-get -q update",
   }
 }
